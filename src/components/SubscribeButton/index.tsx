7d038329e7a7047d1653db5a87070f24dd622306
Luan Nascimento <luan.nascimento11@outlook.com>
2021-03-25 23:14:19 -0300

    Integration with stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripejs';
 import styles from './styles.module.scss';
 
 interface SubscribeButonProps {
@@ -5,10 +8,34 @@ interface SubscribeButonProps {
 }
 
 export function SubscribeButton ({priceId}: SubscribeButonProps) {
+  const [session] = useSession()
+
+  async function handleSubscribe () {
+    if(!session) {
+      signIn('github')
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const {sessionId} = response.data;
+
+      const stripe = await getStripeJs();
+
+      stripe.redirectToCheckout({sessionId})
+
+    }catch(err) {
+      alert(err.message)
+    }
+  }
+
   return (
-    <button 
+    <button
       className={styles.subscribeButton}
-      type="button">
+      type="button"
+      onClick={handleSubscribe}
+      >
       Subscribe now
     </button>
   )
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -14,14 +14,30 @@ export default NextAuth({
   ],
 
   callbacks: {
-    async signIn(user,account, profile ) {
+    async signIn(user, account, profile ) {
       const {email} = user;
       try {
         await fauna.query(
-          q.Create(
+         q.If(
+           q.Not(
+             q.Exists(
+               q.Match(
+                 q.Index('user_by_email'),
+                 q.Casefold(user.email)
+               )
+             )
+           ),
+           q.Create(
             q.Collection('users'),
             {data: { email } }
+          ),
+          q.Get(
+            q.Match(
+              q.Index('user_by_email'),
+              q.Casefold(user.email)
+            )
           )
+         )
         )
       return true
 
